21f67068bedd68d9e3b95e6fa0f35a21d7fb37f9
madrara256 <herbertichama@outlook.com>
2021-09-29 13:39:22 +0300

    added Enrollment entity

---
+++ Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        //Navigation Properties
+        public Course  Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
