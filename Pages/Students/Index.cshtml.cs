853f711839bb2ac79d6bcbf3d25278bbc1a48149
madrara256 <herbertichama@outlook.com>
2021-09-29 21:15:41 +0300

    add filter and sort to the index

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -19,11 +19,41 @@ namespace ContosoUniversity.Pages.Students
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+
         public IList<Student> Student { get;set; }
 
-        public async Task OnGetAsync()
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Student.ToListAsync();
+            //using System
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Student = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
