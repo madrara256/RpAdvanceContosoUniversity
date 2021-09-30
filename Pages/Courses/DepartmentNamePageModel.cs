46319bc6f75b9e63c38a28ddfbe988d142f14755
madrara256 <herbertichama@outlook.com>
2021-10-01 00:05:04 +0300

    Created base class for Course Create and Edit

---
+++ Pages/Courses/DepartmentNamePageModel.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Data;
+using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class DepartmentNamePageModel
+    {
+
+        public SelectList DepartmentNameSL { get; set; }
+
+        public void PolpulateDepartmentsDropDownList(SchoolContext _contetx,
+            object selectedDepartment = null)
+        {
+            var departmentsQuery = from d in _contetx.Departments
+                                   orderby d.Name //Sort by name.
+                                   select d;
+
+            DepartmentNameSL = new SelectList(departmentsQuery.AsNoTracking(),
+                "DepartmentID", "Name", selectedDepartment);
+        }
+    }
+}
