492e52042285048fbe94c2ccbf23abf0fd18317d
madrara256 <herbertichama@outlook.com>
2021-10-01 22:13:47 +0300

    IsConcurrencyToken on Department in SchoolContext

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -28,6 +28,9 @@ namespace ContosoUniversity.Data
                 .WithMany(i => i.Courses);
             modelBuilder.Entity<Student>().ToTable(nameof(Student));
             modelBuilder.Entity<Instructor>().ToTable(nameof(Instructor));
+            modelBuilder.Entity<Department>()
+                .Property(d => d.ConcurrencyToken)
+                .IsConcurrencyToken();
         }
     }
 }
