b22e22d1a1628d9f7c6025065ac8a8cbf2371e9a
madrara256 <herbertichama@outlook.com>
2021-10-01 22:48:56 +0300

    add utility class

---
+++ Utility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ContosoUniversity
+{
+    public class Utility
+    {
+       public static string GetLastChar(byte[] token)
+        {
+            return token[7].ToString();
+        }
+    }
+}
